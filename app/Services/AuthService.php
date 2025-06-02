3e01e6e8f76d6ad9ef9b1a6244badec41a6d3f64
glaucobluedev <glauco@sounuv.com>
2025-06-01 21:20:38 -0300

    fix: calling exceptions on AuthService

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -7,7 +7,9 @@ use App\Repositories\UserRepository;
 use Exception;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\UnauthorizedException;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use function PHPUnit\Framework\throwException;
 
@@ -24,7 +26,7 @@ class AuthService
         $existsCompany = $this->userRepository->findByCnpj($data['cnpj']);
 
         if ($existsCompany) {
-            throw new Exception(`Empresa já cadastrada!`);
+            throw new \App\Exceptions\BadRequestException(`Empresa já cadastrada!`);
         }
 
         $createdCompany = $this->companyRepository->create([
@@ -35,7 +37,7 @@ class AuthService
         $existsUser = $this->userRepository->findByEmail($data['email']);
 
         if ($existsUser) {
-            throw new Exception(`Usuário já cadastrado!`);
+            throw new \App\Exceptions\BadRequestException(`Usuário já cadastrado!`);
         }
 
         $createdUser = $this->userRepository . create([
@@ -54,7 +56,7 @@ class AuthService
         $user = $this->userRepository->findByEmail($credentials['email']);
 
         if (!$user || !Hash::check($credentials['password'], $user->password_hash)) {
-            throw new Exception(`Email e/ou senha incorretos!`);
+            throw new \App\Exceptions\UnauthorizedException(`Email e/ou senha incorretos!`);
         }
 
         return JWTAuth::fromUser($user);
