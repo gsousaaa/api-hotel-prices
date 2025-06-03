af80db3207aed8bedf270d44ea3350aaede2ce09
glaucobluedev <glauco@sounuv.com>
2025-06-03 13:01:14 -0300

    feat: create all tables migration

---
+++ database/migrations/2025_06_03_125735_create_all_tables.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Tabela companies
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cnpj')->unique();
+            $table->string('uf', 2)->nullable();
+            $table->timestamp('created_at')->useCurrent();
+        });
+
+        // Tabela users
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['ADMIN', 'MANAGER', 'EMPLOYEE']);
+            $table->unsignedBigInteger('company_id');
+            $table->timestamp('created_at')->useCurrent();
+
+            // Foreign Key
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+
+        // Tabela rooms
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('type', ['Standard', 'Deluxe', 'Suite']);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+            $table->unsignedBigInteger('company_id');
+
+            // Foreign Key
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+
+        // Tabela prices
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('room_id');
+            $table->decimal('price', 10, 2);
+            $table->date('effective_date');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+
+            // Foreign Key
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prices');
+        Schema::dropIfExists('rooms');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('companies');
+    }
+};
