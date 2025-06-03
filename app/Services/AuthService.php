bbaa0b9f1d5c30ead3ec1da1a4291c6c55e9dfd9
glaucobluedev <glauco@sounuv.com>
2025-06-03 18:04:28 -0300

    fix: AuthService

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,6 +3,8 @@
 namespace App\Services;
 
 use App\Enums\UserRole;
+use App\Exceptions\BadRequestException as BadRequest;
+use App\Repositories\CompanyRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -18,29 +20,30 @@ class AuthService
 
     public function register(array $data)
     {
-        $existsCompany = $this->userRepository->findByCnpj($data['cnpj']);
+        $existsCompany = $this->companyRepository->findByCnpj($data['cnpj']);
 
         if ($existsCompany) {
-            throw new \App\Exceptions\BadRequestException(`Empresa já cadastrada!`);
+            throw new BadRequest(`Empresa já cadastrada!`);
         }
 
         $createdCompany = $this->companyRepository->create([
             'name' => $data['company_name'],
-            'cnpj' => $data['cnpj']
+            'cnpj' => $data['cnpj'],
+            'uf' => $data['uf']
         ]);
 
         $existsUser = $this->userRepository->findByEmail($data['email']);
 
         if ($existsUser) {
-            throw new \App\Exceptions\BadRequestException(`Usuário já cadastrado!`);
+            throw new BadRequest(`Usuário já cadastrado!`);
         }
 
-        $createdUser = $this->userRepository . create([
+        $createdUser = $this->userRepository->create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password_hash' => Hash::make($data['password']),
+            'password' => Hash::make($data['password']),
             'role' => UserRole::MANAGER->value,
-            'company_id' => $createdCompany->id
+            'company_id' => $createdCompany['id']
         ]);
 
         return JWTAuth::fromUser($createdUser);
@@ -50,8 +53,8 @@ class AuthService
     {
         $user = $this->userRepository->findByEmail($credentials['email']);
 
-        if (!$user || !Hash::check($credentials['password'], $user->password_hash)) {
-            throw new \App\Exceptions\UnauthorizedException(`Email e/ou senha incorretos!`);
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            throw new BadRequest('Email e/ou senha incorretos!');
         }
 
         return JWTAuth::fromUser($user);
