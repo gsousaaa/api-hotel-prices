5666d1b55202438de484edbde86c7365e6a6d6ad
glaucobluedev <glauco@sounuv.com>
2025-06-04 21:52:57 -0300

    feat: GET /rooms

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\ApiController;
 
 Route::middleware(['auth.jwt'])->post('/rooms/{id}/price', [ApiController::class, 'calculatePriceForecast']);
 
-
+Route::middleware(['auth.jwt'])->get('/rooms', [ApiController::class, 'getRooms']);
